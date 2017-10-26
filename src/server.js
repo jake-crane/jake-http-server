65522e500184816e25f1b7841ecf1ade9dc64c8a
Jake Crane <jacrane@dstsystems.com>
2017-10-26 08:34:13 -0500

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const STATIC_ROOT = process.argv[2] || 'src';
+const PORT = process.argv[3] || 80;
+
+// console.log(`Serving STATIC_ROOT=${STATIC_ROOT}`);
+// console.log(`Serving PORT=${PORT}`);
+
+app.use(express.static(STATIC_ROOT));
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
